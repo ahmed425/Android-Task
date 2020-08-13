582e4342157f379668592cc79afc5bef082396ef
Ahmed Elsayed <ahmed_elsayed7071@outlook.com>
2020-08-13 21:02:29 +0200

    Implementing Recycler View

--- app/src/main/java/com/example/madarsofttask/ui/ReadUserData.java
+++ app/src/main/java/com/example/madarsofttask/ui/ReadUserData.java
@@ -5,48 +5,84 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.madarsofttask.UserAdapter;
 import com.example.madarsofttask.R;
 import com.example.madarsofttask.models.User;
 import com.example.madarsofttask.viewmodels.UserViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 
 public class ReadUserData extends AppCompatActivity {
-private TextView txtInfo;
+private RecyclerView recyclerView;
+    private TextView txtInfo;
+   private UserAdapter UserAdapter;
 private UserViewModel myUserViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_user_data);
-
-        txtInfo=findViewById(R.id.txt_display_user_info);
-
-        myUserViewModel= ViewModelProviders.of(this).get(UserViewModel.class);
-myUserViewModel.init();
-        myUserViewModel.getUsers().observe(this, new Observer<List<User>>() {
-
-    @Override
-    public void onChanged(@Nullable List<User> users) {
-        String info="";
-
-        for(User user:users){
-            double age=user.getAge();
-            String name=user.getName();
-            String title=user.getJobTitle();
-            String gender=user.getGender();
-info=info+"\n\n"+"  Name : "+name+"\n  Age :"+age+"\n"+"  Job Title :"+title+"\n"+"  Gender :"+gender;
-        }
-        txtInfo.setText(info);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        final UserAdapter UserAdapter = new UserAdapter();
+        recyclerView.setAdapter(UserAdapter);
+        myUserViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+        myUserViewModel.getAllUsers().observe(this, new Observer<List<User>>() {
+            @Override
+            public void onChanged(@Nullable List<User> users) {
+                UserAdapter.setUsers(users);
+            }
+        });
     }
-});
+
+
+        //recyclerView=findViewById(R.id.recycler_view);
+//
+////List<User> myUsersList=new ArrayList<>();
+////        txtInfo=findViewById(R.id.txt_display_user_info);
+//
+//        myUserViewModel= ViewModelProviders.of(this).get(UserViewModel.class);
+//myUserViewModel.init();
+//UserAdapter=new UserAdapter(myUserViewModel.getUsers().getValue());
+//        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+//        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+//        recyclerView.setLayoutManager(layoutManager);
+//        recyclerView.setAdapter(UserAdapter);
+//        myUserViewModel.getUsers().observe(this, new Observer<List<User>>() {
+//
+//    @Override
+//    public void onChanged(@Nullable List<User> users) {
+////        initRecyclerView();
+//
+//        //        String info="";
+//
+////        for(User user:users){
+////            double age=user.getAge();
+////            String name=user.getName();
+////            String title=user.getJobTitle();
+////            String gender=user.getGender();
+////info=info+"\n\n"+"  Name : "+name+"\n  Age :"+age+"\n"+"  Job Title :"+title+"\n"+"  Gender :"+gender;
+////        }
+////        txtInfo.setText(info);
+//        System.out.println("HI"+users.toArray());
+//
+//        UserAdapter.notifyDataSetChanged();
+//    }
+//});
 //        List<User> users=AddUser.myAppDatabase.myDAO().getUsers();
 //
 //        txtInfo.setText(info);
 
-    }
+
+//    private void initRecyclerView(){
+
+//    }
 }
--- app/src/main/java/com/example/madarsofttask/viewmodels/UserViewModel.java
+++ app/src/main/java/com/example/madarsofttask/viewmodels/UserViewModel.java
@@ -15,17 +15,36 @@ import java.util.List;
 
 import static com.example.madarsofttask.ui.AddUser.myAppDatabase;
 
-public class UserViewModel extends ViewModel {
+
+public class UserViewModel extends AndroidViewModel {
+//    private UserRepository repository;
+    private LiveData<List<User>> allUsers;
+
     private MutableLiveData<List<User>> mUsers;
-private UserRepository mUserRepo;
+    private UserRepository mUserRepo;
     public void init(){
-        if(mUsers!=null){
-            return;
-        }
+//        if(mUsers!=null){
+//            return;
+//        }
         mUserRepo=UserRepository.getInstance();
-        mUsers=mUserRepo.getUsers();
+        allUsers=mUserRepo.getUsers();
     }
-public void addUser(String name,String title,double age,String gender){
+    public UserViewModel(@NonNull Application application) {
+        super(application);
+        mUserRepo=UserRepository.getInstance();
+        allUsers=mUserRepo.getUsers();
+//        allUsers=mUserRepo.getUsers();
+
+//        repository = new UserRepository(application);
+//        allUsers = repository.getAllUsers();
+    }
+
+    public LiveData<List<User>> getAllUsers() {
+        System.out.println("Users are : "+allUsers.toString());
+        return allUsers;
+    }
+
+    public void addUser(String name,String title,double age,String gender){
     User user =new User();
     user.setName(name);
     user.setJobTitle(title);
@@ -33,7 +52,26 @@ public void addUser(String name,String title,double age,String gender){
     user.setGender(gender);
     myAppDatabase.myDAO().addUser(user);
 }
-    public LiveData<List<User>> getUsers(){
-        return mUsers;
-    }
 }
+//public class UserViewModel extends ViewModel {
+//    private MutableLiveData<List<User>> mUsers;
+//private UserRepository mUserRepo;
+//    public void init(){
+//        if(mUsers!=null){
+//            return;
+//        }
+//        mUserRepo=UserRepository.getInstance();
+//        mUsers=mUserRepo.getUsers();
+//    }
+//public void addUser(String name,String title,double age,String gender){
+//    User user =new User();
+//    user.setName(name);
+//    user.setJobTitle(title);
+//    user.setAge(age);
+//    user.setGender(gender);
+//    myAppDatabase.myDAO().addUser(user);
+//}
+//    public LiveData<List<User>> getUsers(){
+//        return mUsers;
+//    }
+//}
